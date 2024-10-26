32be999b17d96ea60f97372869d0688be5449d6e
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2024-10-27 01:07:27 +0200

    separate spec into another file

---
+++ cmd/myinterpreter/spec.go
@@ -0,0 +1,154 @@
+package main
+
+// MARK: - Token types
+
+type TokenType int
+const (
+	// Single-character tokens
+	LEFT_PAREN = iota
+	RIGHT_PAREN
+	LEFT_BRACE
+	RIGHT_BRACE
+	COMMA
+	DOT
+	MINUS
+	PLUS
+	SEMICOLON
+	SLASH
+	STAR
+	// Single- or double-character tokens
+	BANG
+	BANG_EQUAL
+	EQUAL
+	EQUAL_EQUAL
+	GREATER
+	GREATER_EQUAL
+	LESS
+	LESS_EQUAL
+	// Literals
+	IDENTIFIER
+	STRING
+	NUMBER
+	// Keywords
+	AND
+	CLASS
+	ELSE
+	FALSE
+	FUN
+	FOR
+	IF
+	NIL
+	OR
+	PRINT
+	RETURN
+	SUPER
+	THIS
+	TRUE
+	VAR
+	WHILE
+	// No-character tokens
+	EOF
+)
+
+func (tokenType TokenType) String() string {
+	switch tokenType {
+	case LEFT_PAREN:
+		return "LEFT_PAREN"
+	case RIGHT_PAREN:
+		return "RIGHT_PAREN"
+	case LEFT_BRACE:
+		return "LEFT_BRACE"
+	case RIGHT_BRACE:
+		return "RIGHT_BRACE"
+	case COMMA:
+		return "COMMA"
+	case DOT:
+		return "DOT"
+	case MINUS:
+		return "MINUS"
+	case PLUS:
+		return "PLUS"
+	case SEMICOLON:
+		return "SEMICOLON"
+	case SLASH:
+		return "SLASH"
+	case STAR:
+		return "STAR"
+	case BANG:
+		return "BANG"
+	case BANG_EQUAL:
+		return "BANG_EQUAL"
+	case EQUAL:
+		return "EQUAL"
+	case EQUAL_EQUAL:
+		return "EQUAL_EQUAL"
+	case GREATER:
+		return "GREATER"
+	case GREATER_EQUAL:
+		return "GREATER_EQUAL"
+	case LESS:
+		return "LESS"
+	case LESS_EQUAL:
+		return "LESS_EQUAL"
+	case IDENTIFIER:
+		return "IDENTIFIER"
+	case STRING:
+		return "STRING"
+	case NUMBER:
+		return "NUMBER"
+	case AND:
+		return "AND"
+	case CLASS:
+		return "CLASS"
+	case ELSE:
+		return "ELSE"
+	case FALSE:
+		return "FALSE"
+	case FUN:
+		return "FUN"
+	case FOR:
+		return "FOR"
+	case IF:
+		return "IF"
+	case NIL:
+		return "NIL"
+	case OR:
+		return "OR"
+	case PRINT:
+		return "PRINT"
+	case RETURN:
+		return "RETURN"
+	case SUPER:
+		return "SUPER"
+	case THIS:
+		return "THIS"
+	case TRUE:
+		return "TRUE"
+	case VAR:
+		return "VAR"
+	case WHILE:
+		return "WHILE"
+	case EOF:
+		return "EOF"
+	}
+	return "?"
+}
+
+var keywords = map[string]TokenType{
+	"and": AND,
+	"class": CLASS,
+	"else": ELSE,
+	"false": FALSE,
+	"fun": FUN,
+	"for": FOR,
+	"if": IF,
+	"nil": NIL,
+	"or": OR,
+	"print": PRINT,
+	"return": RETURN,
+	"super": SUPER,
+	"this": THIS,
+	"true": TRUE,
+	"var": VAR,
+	"while": WHILE,
+}
