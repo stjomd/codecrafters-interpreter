8bc6bd93a22a89f89d6e4bb2b4e69711290f09cf
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2024-10-27 01:00:44 +0200

    tokenize keywords

--- cmd/myinterpreter/tokenizer.go
+++ cmd/myinterpreter/tokenizer.go
@@ -7,90 +7,6 @@ import (
 	"unicode"
 )
 
-// MARK: - Token types
-
-type TokenType int
-const (
-	// Single-character tokens
-	LEFT_PAREN = iota
-	RIGHT_PAREN
-	LEFT_BRACE
-	RIGHT_BRACE
-	COMMA
-	DOT
-	MINUS
-	PLUS
-	SEMICOLON
-	SLASH
-	STAR
-	// Single- or double-character tokens
-	BANG
-	BANG_EQUAL
-	EQUAL
-	EQUAL_EQUAL
-	GREATER
-	GREATER_EQUAL
-	LESS
-	LESS_EQUAL
-	// Literals
-	IDENTIFIER
-	STRING
-	NUMBER
-	// No-character tokens
-	EOF
-)
-func (tokenType TokenType) String() string {
-	switch tokenType {
-	case LEFT_PAREN:
-		return "LEFT_PAREN"
-	case RIGHT_PAREN:
-		return "RIGHT_PAREN"
-	case LEFT_BRACE:
-		return "LEFT_BRACE"
-	case RIGHT_BRACE:
-		return "RIGHT_BRACE"
-	case COMMA:
-		return "COMMA"
-	case DOT:
-		return "DOT"
-	case MINUS:
-		return "MINUS"
-	case PLUS:
-		return "PLUS"
-	case SEMICOLON:
-		return "SEMICOLON"
-	case SLASH:
-		return "SLASH"
-	case STAR:
-		return "STAR"
-	case BANG:
-		return "BANG"
-	case BANG_EQUAL:
-		return "BANG_EQUAL"
-	case EQUAL:
-		return "EQUAL"
-	case EQUAL_EQUAL:
-		return "EQUAL_EQUAL"
-	case GREATER:
-		return "GREATER"
-	case GREATER_EQUAL:
-		return "GREATER_EQUAL"
-	case LESS:
-		return "LESS"
-	case LESS_EQUAL:
-		return "LESS_EQUAL"
-	case IDENTIFIER:
-		return "IDENTIFIER"
-	case STRING:
-		return "STRING"
-	case NUMBER:
-		return "NUMBER"
-	case EOF:
-		return "EOF"
-	}
-	return "?"
-}
-
 // MARK: - Tokens
 
 type Token struct {
@@ -169,7 +85,7 @@ func tokenize(input string) ([]Token, []error) {
 			index := handleNumber(&tokens, &runes, i)
 			i = index - 1
 		} else if unicode.IsLetter(char) || char == '_' {
-			index := handleIdentifier(&tokens, &runes, i)
+			index := handleIdentifierAndKeyword(&tokens, &runes, i)
 			i = index - 1
 		// MARK: Miscellaneous
 		} else if char == '\n' {
@@ -189,11 +105,16 @@ func tokenize(input string) ([]Token, []error) {
 // MARK: - Helper functions
 
 // Identifier handling
-func handleIdentifier(tokens *[]Token, runes *[]rune, currentPosition int) int {
+func handleIdentifierAndKeyword(tokens *[]Token, runes *[]rune, currentPosition int) int {
 	slice := *runes
 	index := skipUntil(runes, currentPosition + 1, IS_IDENTIFIER_END)
 	lexeme := string(slice[currentPosition:index])
-	*tokens = append(*tokens, Token{Type: IDENTIFIER, Lexeme: lexeme, Literal: "null"})
+	keywordTokenType, presentInKeywords := keywords[lexeme]
+	if presentInKeywords {
+		*tokens = append(*tokens, Token{Type: keywordTokenType, Lexeme: lexeme, Literal: "null"})
+	} else {
+		*tokens = append(*tokens, Token{Type: IDENTIFIER, Lexeme: lexeme, Literal: "null"})
+	}
 	return index
 }
 
