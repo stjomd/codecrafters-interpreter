0559cd259e63448b566602863552552edf006c09
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2024-10-30 00:06:12 +0100

    refactor

---
+++ cmd/myinterpreter/expr.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// MARK: - Expressions
+
+type Expr interface {
+	String() string
+}
+
+type LiteralExpr struct {
+	value any
+}
+func (le LiteralExpr) String() string {
+	if le.value == nil {
+		return "nil"
+	} else if reflect.TypeOf(le.value).Kind() == reflect.Float64 {
+		return Float64ToString(le.value.(float64))
+	} else {
+		return fmt.Sprint(le.value)
+	}
+}
+
+type GroupingExpr struct {
+	expr Expr
+}
+func (ge GroupingExpr) String() string {
+	return fmt.Sprintf("(group %v)", ge.expr)
+}
+
+type UnaryExpr struct {
+	operation Token
+	expr Expr
+}
+func (ue UnaryExpr) String() string {
+	return fmt.Sprintf("(%v %v)", ue.operation.Lexeme, ue.expr)
+}
+
+type BinaryExpr struct {
+	left Expr
+	operation Token
+	right Expr
+}
+func (be BinaryExpr) String() string {
+	return fmt.Sprintf("(%v %v %v)", be.operation.Lexeme, be.left, be.right)
+}
