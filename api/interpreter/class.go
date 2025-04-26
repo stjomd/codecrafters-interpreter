137d95f91a80f614026adbb0f5bb648b33de7e48
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2025-04-26 17:20:49 +0200

    codecrafters submit [skip ci]

--- api/interpreter/class.go
+++ api/interpreter/class.go
@@ -1,8 +1,24 @@
 package interpreter
 
-type Class struct {
+type Class struct { // implements Callable
 	Name string
 }
 func (class Class) String() string {
 	return class.Name
+}
+
+type ClassInstance struct {
+	Class *Class
+}
+func (instance ClassInstance) String() string {
+	return instance.Class.Name + " instance"
+}
+
+// MARK: - Class Callable
+func (class Class) arity() int {
+	return 0
+}
+func (class Class) call(intp *Interpreter, args []any) (any, error) {
+	instance := ClassInstance{Class: &class}
+	return instance, nil
 }
